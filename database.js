fbcd2c7456745857988d27e5bdf46e441f110c8d
I R Nut Smurt <73799093+irnutsmurt@users.noreply.github.com>
2023-11-07 10:50:44 -0800

    Update database.js
    Chat history and response from openai so that you can have a conversation with the bot finally.

--- database.js
+++ database.js
@@ -2,26 +2,94 @@ const sqlite3 = require('sqlite3').verbose();
 
 class Database {
   constructor() {
-    this.db = new sqlite3.Database('./questions.db', (err) => {
+    this.db = new sqlite3.Database('./conversation.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
       if (err) {
         console.error(err.message);
       } else {
-        console.log('Connected to the questions database.');
+        console.log('Connected to the conversation database.');
+        this.initializeDatabase();
       }
     });
+  }
 
+  initializeDatabase() {
     this.db.run(`
-      CREATE TABLE IF NOT EXISTS questions (
-        id INTEGER PRIMARY KEY,
-        user_id TEXT,
-        question TEXT
+      CREATE TABLE IF NOT EXISTS Questions (
+        MessageID INTEGER PRIMARY KEY,
+        UserID INTEGER,
+        QuestionContent TEXT,
+        Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+  this.db.run(`
+    CREATE TABLE IF NOT EXISTS History (
+      UserID INTEGER,
+      MessageID INTEGER,
+      QuestionContent TEXT,
+      ResponseContent TEXT,
+      Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (UserID, MessageID)
+    );
+  `);
+    
+    this.db.run(`
+      CREATE TABLE IF NOT EXISTS Responses (
+        ResponseID INTEGER PRIMARY KEY AUTOINCREMENT,
+        MessageID INTEGER,
+        ResponseContent TEXT,
+        Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (MessageID) REFERENCES Questions(MessageID)
       );
     `);
   }
 
-  saveQuestion(userId, question) {
+  saveQuestion(messageId, userId, questionContent) {
     return new Promise((resolve, reject) => {
-      this.db.run('INSERT INTO questions (user_id, question) VALUES (?, ?)', [userId, question], (err) => {
+      this.db.run('INSERT INTO Questions (MessageID, UserID, QuestionContent) VALUES (?, ?, ?)', [messageId, userId, questionContent], function(err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this.lastID);
+        }
+      });
+    });
+  }
+
+  saveResponse(messageId, responseContent) {
+    return new Promise((resolve, reject) => {
+      this.db.run('INSERT INTO Responses (MessageID, ResponseContent) VALUES (?, ?)', [messageId, responseContent], function(err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this.lastID);
+        }
+      });
+    });
+  }
+
+  getConversationHistory(userId) {
+    return new Promise((resolve, reject) => {
+      this.db.all('SELECT q.QuestionContent, r.ResponseContent FROM Questions q LEFT JOIN Responses r ON q.MessageID = r.MessageID WHERE q.UserID = ? ORDER BY q.Timestamp', [userId], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+
+  archiveHistory(userId) {
+    return new Promise((resolve, reject) => {
+      const query = `
+        INSERT INTO History (UserID, MessageID, QuestionContent, ResponseContent, Timestamp)
+        SELECT q.UserID, q.MessageID, q.QuestionContent, r.ResponseContent, q.Timestamp
+        FROM Questions q
+        LEFT JOIN Responses r ON q.MessageID = r.MessageID
+        WHERE q.UserID = ?;
+      `;
+      this.db.run(query, [userId], (err) => {
         if (err) {
           reject(err);
         } else {
@@ -31,18 +99,26 @@ class Database {
     });
   }
 
-  getLastQuestion(userId) {
+  clearHistory(userId) {
     return new Promise((resolve, reject) => {
-      const query = 'SELECT question FROM questions WHERE user_id = ? ORDER BY id DESC LIMIT 1';
-      this.db.get(query, [userId], (err, row) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(row ? row.question : '');
-        }
+      this.db.serialize(() => {
+        this.db.run('DELETE FROM Questions WHERE UserID = ?', [userId], (err) => {
+          if (err) {
+            return reject(err);
+          }
+        });
+        this.db.run('DELETE FROM Responses WHERE MessageID IN (SELECT MessageID FROM Questions WHERE UserID = ?)', [userId], (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
       });
     });
   }
 }
+
+
 
 module.exports = { Database };
